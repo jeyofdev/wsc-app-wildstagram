0b0223e39ca513c022e4c6c05fab0f2fb5b61a28
jeyofdev <jgregoire.pro@gmail.com>
2022-10-02 19:09:41 +0200

    feat : show the images on screen ImagesScreen

--- screens/ImagesScreen.tsx
+++ screens/ImagesScreen.tsx
@@ -1,7 +1,58 @@
-import { Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import * as FileSystem from 'expo-file-system';
+import { FlatList, Image, StyleSheet, Text } from 'react-native';
 
 const ImagesScreen = () => {
-    return <Text>ImagesScreen</Text>;
+    const [imagesList, setImagesList] = useState<string[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            const images = await FileSystem.readDirectoryAsync(
+                (FileSystem.cacheDirectory as string) + 'ImageManipulator'
+            );
+
+            console.log('images', images);
+            setImagesList(images);
+        })();
+    }, []);
+
+    return (
+        <>
+            <Text>ImagesScreen</Text>
+
+            {imagesList.length > 0 ? (
+                <FlatList
+                    data={imagesList}
+                    keyExtractor={(imageData) => imageData}
+                    renderItem={(itemData) => {
+                        console.log('imageeeeeeee', itemData);
+
+                        return (
+                            <>
+                                <Image
+                                    style={styles.image}
+                                    source={{
+                                        uri:
+                                            FileSystem.cacheDirectory +
+                                            'ImageManipulator/' +
+                                            itemData.item,
+                                    }}
+                                />
+                            </>
+                        );
+                    }}
+                />
+            ) : null}
+        </>
+    );
 };
+
+const styles = StyleSheet.create({
+    image: {
+        resizeMode: 'cover',
+        height: 100,
+        marginBottom: 16,
+    },
+});
 
 export default ImagesScreen;
